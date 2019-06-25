72573e8973094db470f5619f3772b872982eef93
Fatema-binbytes <fatema.binbytes@gmail.com>
2019-06-25 19:11:20 +0530

    app format

--- src/screen/App.js
+++ src/screen/App.js
@@ -1,41 +1,40 @@
 
 import React, {Component} from 'react';
 import { TextInput, View,Button} from 'react-native';
-
 import {connect } from 'react-redux'
 
 import {user} from '../store/action'
 import TextComponent from '../components/text'
 import styles from './app-style'
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
-    this.state={toList:''}
-    
+    this.state = {
+      toList:''
+    }
   }
   render() {
     return (
       <View style={styles.container}>
-         <TextInput placeholder="Enter Name" onChangeText={(text) => this.setState({toList:text})}/>
-         <TextComponent style={styles.txt} text={this.props.user} />
-         <Button title={"click"} onPress={()=>this.props.Add(this.state.toList)}/>
-            
+        <TextInput placeholder="Enter Name" onChangeText={(text) => this.setState({toList:text})}/>
+        <TextComponent style={styles.txt} text={this.props.user} />
+        <Button title={"click"} onPress={()=>this.props.Add(this.state.toList)}/>
       </View>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-    user : state.user
+  user : state.user
 })
 const mapDispatchToProps = ({
-   Add :user.intName
+  Add : user.intName
 })
 
-const AppWrapper = connect(
+const AppWrapper = connect (
     mapStateToProps,
     mapDispatchToProps
   )(App)
 
-  export default AppWrapper
+export default AppWrapper
